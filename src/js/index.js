ecbc9109f686f062c22203b090a860d7387eba78
juliaret <julia.retuertanogales@alumnostrazos.net>
2024-07-08 09:53:04 +0200

    clase-08-julio

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,53 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const allTasks = [
+  {
+    id: Date.now(),
+    task: 'Comprar el pan',
+    completed: false
+  }
+];
+
+const formElement = document.getElementById('form');
+const inputTextTaskElement = document.getElementById('input-text-task');
+const tasksListElement = document.getElementById('tasks-list');
+
+const insertTasks = tasks => {
+  const fragment = document.createDocumentFragment();
+
+  allTasks.forEach(task => {
+    const newLi = document.createElement('li');
+    newLi.textContent = task.task;
+
+    const newCheckbox = document.createElement('input');
+    newCheckbox.setAttribute('type', 'checkbox');
+    newCheckbox.checked = task.completed;
+
+    newCheckbox.addEventListener('change');
+
+    newLi.append(newCheckbox);
+    fragment.append(newLi);
+  });
+
+  tasksListElement.textContent = '';
+  tasksListElement.append(fragment);
+};
+
+formElement.addEventListener('submit', addTask);
+
+const createTask = task => {
+  const taskContent = inputTextTaskElement.value;
+
+  if (taskContent !== '') {
+    const newTask = {
+      id: Date.now(),
+      task: taskContent,
+      completed: false
+    };
+    allTasks.push(newTask);
+    inputTextTaskElement.value = '';
+  }
+};
+
+insertTasks(allTasks);
